7968cc07ff8cb8700519fa7ed85229e385f0b941
Jonathan Bobrow <jbobrow@gmail.com>
2021-03-10 23:50:53 -0500

    Update id.ino
    First cleanup. Take the click listener out of the for loop. The click only needs to be listened for once, not for each face.
    
    Then, added a variable for our own Id index, since each Blink needs to know which of the ids is theirs.
    
    Created an unassigned value (could have used a boolean to determine if we have received an id yet also, but checking to see if we still == UNASSIGNED feels more straightforward)
    
    Only look to neighbors for assignment if I am in fact, UNASSIGNED
    
    Then the display. No need to loop through the array, since we simply want to display our own id, which we can access from the index we have been assigned.
    
    Add a display state for when we are unassigned as well, for fun, let's us know the Blinks are on and working.

--- Projects/Something Familiar/Carly/id/id.ino
+++ Projects/Something Familiar/Carly/id/id.ino
@@ -1,79 +1,94 @@
+byte ids[10] = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
 
+#define UNASSIGNED 10 // this is out of bound for the array
 
-byte id [10] = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
+byte myIdIndex = UNASSIGNED;  // to store which one in the array is ours
 
 Color PURPLE = makeColorRGB(180, 74, 255);
 
 void setup() {
   // put your setup code here, to run once:
-   setValueSentOnAllFaces(0);
+  setValueSentOnAllFaces(0);
 
 }
 
-void loop() { 
+void loop() {
 
+  // listen for a button click
+  if (buttonSingleClicked() ) {
+    myIdIndex = 0;  // I'm first in line
+    setValueSentOnAllFaces(1);  // tell my neighbors
+  }
 
-   FOREACH_FACE(f) {
-     if (buttonSingleClicked() ) {
-       id[0];
-       setValueSentOnAllFaces(1); 
-      }
-    if (!isValueReceivedOnFaceExpired(f)) {
-      if (getLastValueReceivedOnFace(f) == 1) {
-        id[1];
-        setValueSentOnAllFaces(2);
-       }
-       if (getLastValueReceivedOnFace(f) == 2) {
-        id[2];
-        setValueSentOnAllFaces(3);
-       }
-       if (getLastValueReceivedOnFace(f) == 3) {
-        id[3];
-        setValueSentOnAllFaces(4);
-       }
-       if (getLastValueReceivedOnFace(f) == 4) {
-        id[4];
-        setValueSentOnAllFaces(5);
-       }
-       if (getLastValueReceivedOnFace(f) == 5) {
-        id[5];
-        setValueSentOnAllFaces(6);
-       }
-       if (getLastValueReceivedOnFace(f) == 6) {
-        id[6];
-        setValueSentOnAllFaces(7);
-       }
-       if (getLastValueReceivedOnFace(f) == 7) {
-        id[7];
-        setValueSentOnAllFaces(8);
-       }
-       if (getLastValueReceivedOnFace(f) == 8) {
-        id[8];
-        setValueSentOnAllFaces(9);
-       }
-       if (getLastValueReceivedOnFace(f) == 9) {
-        id[9];
-        setValueSentOnAllFaces(10);
-       }
-     }
-   }
-  
-
-   //display colors 
-   FOREACH_FACE(f) { 
-      byte len = sizeof(id)/sizeof(id[0]);
-      for (byte i = 0 ; i < len ; i++) {
-        if (id[i] = 1) {
-          setColor(MAGENTA);
-        } else if (id[i] = 2) {
-          setColor(BLUE);
-        } else if (id[i] = 3) {
-          setColor(GREEN);
-        } else if (id[i] = 4) {
-          setColor(YELLOW);
-        } else if (id[i] = 5) {
-          setColor(PURPLE);
+  // listen to neighbors for assignment
+  if (myIdIndex == UNASSIGNED) {  // only assign if not yet assigned
+    FOREACH_FACE(f) {
+      if (!isValueReceivedOnFaceExpired(f)) { // If I have a neighbor here do the following
+        if (getLastValueReceivedOnFace(f) == 1) {
+          myIdIndex = 1;
+          setValueSentOnAllFaces(2);
         }
-      } 
+        else if (getLastValueReceivedOnFace(f) == 2) {
+          myIdIndex = 2;
+          setValueSentOnAllFaces(3);
+        }
+        else if (getLastValueReceivedOnFace(f) == 3) {
+          myIdIndex = 3;
+          setValueSentOnAllFaces(4);
+        }
+        else if (getLastValueReceivedOnFace(f) == 4) {
+          myIdIndex = 4;
+          setValueSentOnAllFaces(5);
+        }
+        else if (getLastValueReceivedOnFace(f) == 5) {
+          myIdIndex = 5;
+          setValueSentOnAllFaces(6);
+        }
+        else if (getLastValueReceivedOnFace(f) == 6) {
+          myIdIndex = 6;
+          setValueSentOnAllFaces(7);
+        }
+        else if (getLastValueReceivedOnFace(f) == 7) {
+          myIdIndex = 7;
+          setValueSentOnAllFaces(8);
+        }
+        else if (getLastValueReceivedOnFace(f) == 8) {
+          myIdIndex = 8;
+          setValueSentOnAllFaces(9);
+        }
+        else if (getLastValueReceivedOnFace(f) == 9) {
+          myIdIndex = 9;
+          setValueSentOnAllFaces(10);
+        }
+      }
     }
+  }
+
+
+  //display colors
+
+  if (myIdIndex != UNASSIGNED) {
+    // display my assigned color
+
+    if (ids[myIdIndex] == 1) {
+      setColor(MAGENTA);
+    } else if (ids[myIdIndex] == 2) {
+      setColor(BLUE);
+    } else if (ids[myIdIndex] == 3) {
+      setColor(GREEN);
+    } else if (ids[myIdIndex] == 4) {
+      setColor(YELLOW);
+    } else if (ids[myIdIndex] == 5) {
+      setColor(PURPLE);
+    }
+  }
+  else {
+    // display a rainbow when not yet assigned
+    setColorOnFace(RED, 0);
+    setColorOnFace(ORANGE, 1);
+    setColorOnFace(YELLOW, 2);
+    setColorOnFace(GREEN, 3);
+    setColorOnFace(BLUE, 4);
+    setColorOnFace(MAGENTA, 5);
+  }
 }
