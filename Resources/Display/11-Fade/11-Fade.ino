7cf6cad13d3b9e9f6197a3200c9e9649c0c487ee
Jonathan Bobrow <jbobrow@gmail.com>
2021-04-06 19:16:12 -0400

    Update 11-Fade.ino
    Adding a fade example to the display resources

--- Resources/Display/11-Fade/11-Fade.ino
+++ Resources/Display/11-Fade/11-Fade.ino
@@ -5,8 +5,45 @@
     --------------------
 */
 
-void setup() { 
+byte faceBri[6];
+
+#define DECREMENT 5
+#define FADE_INTERVAL 20
+
+byte index = 0;
+
+Timer fadeTimer;
+
+void setup() {
+  // put your setup code here, to run once:
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (buttonPressed()) {
+    faceBri[index] = 255;
+    index++;
+
+    if (index >= 6) {
+      index = 0;
+    }
+  }
+
+
+  if (fadeTimer.isExpired()) {
+
+    FOREACH_FACE(f) {
+      if (faceBri[f] > DECREMENT) {
+        faceBri[f] -= DECREMENT;
+      }
+    }
+    fadeTimer.set(FADE_INTERVAL);
+  }
+
+
+  // display our bri
+  FOREACH_FACE(f) {
+    setColorOnFace(dim(WHITE, faceBri[f]), f);
+  }
 }
