ee795e149ecb6deda681e139593060f98109db85
Jonathan Bobrow <jbobrow@gmail.com>
2021-05-05 17:21:14 -0400

    Updating the delay spread animation example

--- Resources/Communication/03-DelaySpread/03-DelaySpread.ino
+++ Resources/Communication/03-DelaySpread/03-DelaySpread.ino
@@ -1,63 +1,99 @@
 /*
-   Let's make Blinks talk to each other
-   Share our value
+   Spreading a message in the most precarious way possible
+
+   Send the message we heard,
+   then don't listen for a period of time
+
+   This is how I used to propogate messages until Dan King created
+   a much more stable solution. This sketch does do what you'd expect
+   but it feels a bit more precarious than the validation that occurs with
+   the Safe Communication tutorials
+
+   by Jonathan Bobrow
+   05.05.2021
 */
 
-Timer waitTimer;
-#define WAIT_DURATION 1000
+#define BROADCAST_DURATION 100
+#define DEAF_DURATION 200
+#define ANIMATION_DELAY 70
 
-bool isPulsing = false;  // 1 = TRUE and 0 = FALSE
+uint32_t timeOfSwitch = 0;
+
+Color colors[] = {WHITE, CYAN, MAGENTA, BLUE, ORANGE, GREEN, RED};
+
+byte myState = 1;
+byte numStates = 6;
+
+byte goToValue = 1;
+Timer goToTimer;
 
 void setup() {
-  // put your setup code here, to run once:
-  waitTimer.never();
+  // nothing needed in setup
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-  // react to user input
+  // increment our state each time the button is pressed
   if (buttonPressed()) {
-    isPulsing = !isPulsing;
+    setState(myState + 1);
   }
 
-  // Listen to my neighbors
-  FOREACH_FACE(f) {
-    // loop through all of them
-    if (!isValueReceivedOnFaceExpired(f)) {
-      // if my neighbor is present, get their value
 
-      byte neighborVal = getLastValueReceivedOnFace(f); // listen
+  // determine if we should listen to our neighbors
+  if (timeOfSwitch < millis() - (BROADCAST_DURATION + DEAF_DURATION) ) {
 
-      // set my value, to be the same as my neighbor
-      if (neighborVal == 1 && waitTimer.isExpired()) {
-        waitTimer.set(WAIT_DURATION);
+    // we have broadcasted and ignored feedback, so we are good to listen again
+    FOREACH_FACE(f) {
+
+      // is there a Blink on this face?
+      if (!isValueReceivedOnFaceExpired(f)) {
+
+        // get their value
+        byte neighbor = getLastValueReceivedOnFace(f);
+
+        // is our neighbor different (since Blinks broadcast 0 by default, we don't want to respond to 0)
+        if (neighbor != 0 && neighbor != myState) {
+          // store neighbor value and change to it after delay
+          goToTimer.set(ANIMATION_DELAY);
+          goToValue = neighbor;
+
+        }
       }
     }
   }
 
-  // wait a period of time before reacting
-  if(waitTimer.isExpired()) {
-    isPulsing = true;
-    waitTimer.never();
+  if (goToTimer.isExpired()) {
+    setState(goToValue);
+    goToTimer.never();
   }
 
-  // Tell everyone if we are pulsing
-  if (isPulsing) {
-    setValueSentOnAllFaces(1);
+  // is the time passed our broadcast duration?
+  if (timeOfSwitch < millis() - BROADCAST_DURATION ) {
+
+    // return to sending 0
+    setValueSentOnAllFaces(0);  // stop sending state
+  }
+
+  // display a color that matches our state
+  setColor(colors[myState]);
+}
+
+
+/*
+   setState
+    - Set the state of the Blink to this value
+    - Broadcast the value of the Blink on all faces
+    - Set the time that we started broadcasting
+*/
+void setState(byte _state) {
+  if (_state >= numStates) {
+    myState = 1;
   }
   else {
-    setValueSentOnAllFaces(0);
+    myState = _state;
   }
 
+  timeOfSwitch = millis();
 
-  // display
-  if(isPulsing) {
-    setColor( dim(WHITE, sin8_C(millis()/2)) );
-  }
-  else {
-    setColor( WHITE );
-  }
-
-  
+  setValueSentOnAllFaces(myState);  // start sending state
 }
